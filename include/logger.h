9ac3cee55646f7966d6b5d96e5d5f8881cb5c76e
sukrisl <laksono.sukri@gmail.com>
2025-12-23 13:50:48 +0700

    refactor: separate log stream functionalities into another file

--- include/logger.h
+++ include/logger.h
@@ -8,27 +8,6 @@
 extern "C" {
 #endif
 
-typedef struct {
-    uint16_t file_index;
-    uint32_t offset;
-} logstream_pointer_t;
-
-typedef struct {
-    uint16_t version;
-
-    uint32_t num_unread_entries;
-
-    logstream_pointer_t head;
-    logstream_pointer_t tail;
-
-    uint32_t crc8;
-} logstream_meta_t;
-
-typedef struct {
-    bool initialized;
-    storage_worker_t storage_handle;
-} logger_t;
-
 // Synchronous operation context (used with storage_worker callbacks)
 typedef struct {
     SemaphoreHandle_t sem;    // shared semaphore to signal completion
@@ -39,29 +18,39 @@ typedef struct {
 } op_sync_t;
 
 typedef struct {
-    char name[16];
-    char dirpath[STORAGE_MAX_PATH];
-    logger_t* logger;
-    logstream_meta_t meta;
-    // Synchronization primitives
-    SemaphoreHandle_t op_sem;      // created in logstream_open, deleted in close
-    SemaphoreHandle_t meta_mutex;  // protects concurrent access to metadata
-    // Per-operation contexts (separated by op type)
-    op_sync_t sync_read;
-    op_sync_t sync_write;
-    op_sync_t sync_append;
-    op_sync_t sync_delete;
-    op_sync_t sync_mkdir;
-} logstream_t;
+    bool initialized;
+    storage_worker_t storage_handle;
+} logger_t;
 
 esp_err_t logger_init(mount_point_t* mount_point, logger_t* out);
 esp_err_t logger_deinit(logger_t* logger);
 
-esp_err_t logstream_open(logger_t* logger, const char* stream_name, logstream_t* out_stream);
-esp_err_t logstream_close(logstream_t* stream);
-esp_err_t logstream_put(logstream_t* stream, const uint8_t* payload, size_t len);
-esp_err_t logstream_get_unread(logstream_t* stream, uint8_t* out, size_t out_size, size_t* bytes_read);
-esp_err_t logstream_get_status(logstream_t* stream, logstream_meta_t* out_meta);
+// TODO: API of log streams
+esp_err_t logger_register_stream(const char* stream_name, logger_t* logger);
+esp_err_t logger_unregister_stream(const char* stream_name, logger_t* logger);
+
+esp_err_t logger_put_entry(logger_t* logger, const char* stream_name, const uint8_t* payload, size_t len);
+esp_err_t logger_get_entries(logger_t* logger, const char* stream_name, uint8_t* out, size_t out_size,
+                             size_t* bytes_read);
+esp_err_t logger_mark_entries_read(logger_t* logger, const char* stream_name, void*);
+
+#define APP_LOG(stream, format, ...)                                                                                \
+    do {                                                                                                            \
+        esp_err_t err = logger_put_entry(stream, (const uint8_t*)format, snprintf(NULL, 0, format, ##__VA_ARGS__)); \
+        if (err != ESP_OK) { /* handle error */                                                                     \
+        }                                                                                                           \
+    } while (0)
+
+#define APP_LOG_GET(stream, out, out_size, bytes_read)                         \
+    do {                                                                       \
+        esp_err_t err = logger_get_entries(stream, out, out_size, bytes_read); \
+        if (err != ESP_OK) { /* handle error */                                \
+        } else {                                                               \
+            err = logger_mark_entries_read(stream, out, out_size, bytes_read); \
+            if (err != ESP_OK) { /* handle error */                            \
+            }                                                                  \
+        }                                                                      \
+    } while (0)
 
 #ifdef __cplusplus
 }
---
+++ include/logstream.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "esp_err.h"
+#include "logger.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+    uint16_t file_index;
+    uint32_t offset;
+} logstream_pointer_t;
+
+typedef struct {
+    uint16_t version;
+
+    uint32_t num_unread_entries;
+
+    logstream_pointer_t head;
+    logstream_pointer_t tail;
+
+    uint32_t crc8;
+} logstream_meta_t;
+
+typedef struct {
+    char name[16];
+    char dirpath[STORAGE_MAX_PATH];
+    logger_t* logger;
+    logstream_meta_t meta;
+    // Synchronization primitives
+    SemaphoreHandle_t op_sem;      // created in logstream_open, deleted in close
+    SemaphoreHandle_t meta_mutex;  // protects concurrent access to metadata
+    // Per-operation contexts (separated by op type)
+    op_sync_t sync_read;
+    op_sync_t sync_write;
+    op_sync_t sync_append;
+    op_sync_t sync_delete;
+    op_sync_t sync_mkdir;
+} logstream_t;
+
+esp_err_t logstream_open(logger_t* logger, const char* stream_name, logstream_t* out_stream);
+esp_err_t logstream_close(logstream_t* stream);
+esp_err_t logstream_put(logstream_t* stream, const uint8_t* payload, size_t len);
+esp_err_t logstream_get_unread(logstream_t* stream, uint8_t* out, size_t out_size, size_t* bytes_read);
+esp_err_t logstream_get_status(logstream_t* stream, logstream_meta_t* out_meta);
+
+#ifdef __cplusplus
+}
+#endif
